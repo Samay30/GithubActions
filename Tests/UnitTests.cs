8af9a421357f198b524d0fff99bd1395b3147c21
Samay Bhojwani <samaybhojwani@MacBookAir.home.local>
2025-10-27 19:50:29 -0500

    Added all 15 unit tests for CI lab

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -3,8 +3,9 @@ using System;
 
 namespace GithubActionsLab
 {
+    // ---------------------- ADD ----------------------
     [TestClass]
-    public class MathTests
+    public class Addition
     {
         [TestMethod]
         public void Add_Valid_Bhojwani()
@@ -29,25 +30,121 @@ namespace GithubActionsLab
             Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
             Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
         }
+    }
 
-        // 🔹 NEW TESTS for Power()
+    // ---------------------- SUBTRACT ----------------------
+    [TestClass]
+    public class Subtraction
+    {
         [TestMethod]
-        public void Power_Valid_Bhojwani()
+        public void Subtract_Valid_Bhojwani()
         {
-            Assert.AreEqual(8, Program.Power("2", "3"), 0.001, "Power(2,3) should return 8");
-            Assert.AreEqual(16, Program.Power("4", "2"), 0.001, "Power(4,2) should return 16");
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(0, Program.Subtract("5", "5"));
         }
 
         [TestMethod]
-        public void Power_Negative_Bhojwani()
+        public void Subtract_Invalid_Bhojwani()
         {
-            Assert.AreEqual(0.25, Program.Power("2", "-2"), 0.001, "Power(2,-2) should return 0.25");
+            Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "2"));
+            Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "b"));
+            Assert.ThrowsException<FormatException>(() => Program.Subtract("x", "y"));
+        }
+
+        [TestMethod]
+        public void Subtract_Null_Bhojwani()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "2"));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+    }
+
+    // ---------------------- MULTIPLY ----------------------
+    [TestClass]
+    public class Multiplication
+    {
+        [TestMethod]
+        public void Multiply_Valid_Bhojwani()
+        {
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
+        }
+
+        [TestMethod]
+        public void Multiply_Invalid_Bhojwani()
+        {
+            Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "2"));
+            Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "b"));
+            Assert.ThrowsException<FormatException>(() => Program.Multiply("x", "y"));
+        }
+
+        [TestMethod]
+        public void Multiply_Null_Bhojwani()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "2"));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+    }
+
+    // ---------------------- DIVIDE ----------------------
+    [TestClass]
+    public class Division
+    {
+        [TestMethod]
+        public void Divide_Valid_Bhojwani()
+        {
+            Assert.AreEqual(2, Program.Divide("4", "2"));
+            Assert.AreEqual(3, Program.Divide("9", "3"));
+            Assert.AreEqual(1, Program.Divide("5", "5"));
+        }
+
+        [TestMethod]
+        public void Divide_Invalid_Bhojwani()
+        {
+            Assert.ThrowsException<FormatException>(() => Program.Divide("a", "2"));
+            Assert.ThrowsException<FormatException>(() => Program.Divide("1", "b"));
+            Assert.ThrowsException<FormatException>(() => Program.Divide("x", "y"));
+        }
+
+        [TestMethod]
+        public void Divide_Null_Bhojwani()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "2"));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+    }
+
+    // ---------------------- POWER ----------------------
+    [TestClass]
+    public class Power
+    {
+        [TestMethod]
+        public void Power_Valid_Bhojwani()
+        {
+            Assert.AreEqual(1, Program.Power("1", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(25, Program.Power("5", "2"));
         }
 
         [TestMethod]
         public void Power_Invalid_Bhojwani()
         {
-            Assert.ThrowsException<FormatException>(() => Program.Power("a", "b"));
+            Assert.ThrowsException<FormatException>(() => Program.Power("a", "2"));
+            Assert.ThrowsException<FormatException>(() => Program.Power("1", "b"));
+            Assert.ThrowsException<FormatException>(() => Program.Power("x", "y"));
+        }
+
+        [TestMethod]
+        public void Power_Null_Bhojwani()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "2"));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
         }
     }
 }
